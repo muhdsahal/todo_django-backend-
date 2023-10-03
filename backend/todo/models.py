b9a607a12eafdb74e76517c5b77f7292850deb72
sahal <sahalshalu830@gmail.com>
2023-10-03 20:34:18 +0530

    todo list using django + react

---
+++ backend/todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self) :
+        return self.title
